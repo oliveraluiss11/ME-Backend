ce6eb77479cc5243d6331e2de8b6080334417d9c
oliveraluiss11 <loliverv11@gmail.com>
2024-12-21 17:56:57 -0500

    Corrección de configuración del yml

--- src/main/java/com/jayway/market_express/common/constant/LocalPathConstant.java
+++ src/main/java/com/jayway/market_express/common/constant/LocalPathConstant.java
@@ -1,5 +1,5 @@
 package com.jayway.market_express.common.constant;
 
 public class LocalPathConstant {
-    public static final String SERVICE_ACCOUNT_KEY = "serviceAccountKey.json";
+    public static final String SERVICE_ACCOUNT_KEY = "/etc/secrets/serviceAccountKey.json";
 }
